05f009072fc108799c815c8aa4bf2332b970322f
amin <94103247+amin-rahmani@users.noreply.github.com>
2022-11-27 15:23:59 +0330

    add a few thing to draw and fix some stuff

--- script.js
+++ script.js
@@ -3,25 +3,32 @@
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
-const width = canvas.width = width.innerWidth;
-const height = canvas.height = height.innerHeight
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight
 
 // function to generate random number
-function random(min,max){
+function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 // function to generate a random RGB color
-function randomRGB(){
-    return `rgb(${random(0,255)},${random(0,255)})`
+function randomRGB() {
+    return `rgb(${random(0, 255)},${random(0, 255)})`
 }
 
 class Ball {
-    constructor(x,y,velX,velY,color,size){
+    constructor(x, y, velX, velY, color, size) {
         this.x = x;
         this.y = y;
         this.velX = velX;
         this.velY = velY;
         this.color = color;
         this.size = size;
+    }
+
+    draw() {
+        ctx.beginPath(); // start drawing shape
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.fill();
     }
 }
