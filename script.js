2d981f3ac1ae1329c01def58c6789f7073a19fd4
amin <94103247+amin-rahmani@users.noreply.github.com>
2022-11-27 18:20:48 +0330

    and finally changes color when they touching eachother

--- script.js
+++ script.js
@@ -54,6 +54,20 @@ class Ball {
         this.x += this.velX;
         this.y += this.velY;
     }
+
+    collisionDetect() {
+        for (const ball of balls) {
+            if (!(this === ball)) {
+                const dx = this.x - ball.x
+                const dy = this.y - ball.y
+                const distance = Math.sqrt(dx * dx + dy * dy)
+
+                if (distance < this.size + ball.size) {
+                    ball.color = this.ball = randomRGB()
+                }
+            }
+        }
+    }
 }
 
 const balls = []
@@ -75,11 +89,12 @@ while (balls.length < 10) {
 }
 
 function loop() {
-    ctx.fillStyle = 'rgba(0,0,0,0.3)'
+    ctx.fillStyle = 'rgba(0,0,0,0.01)'
     ctx.fillRect(0, 0, width, height)
     for (let ball of balls) {
         ball.draw()
         ball.update()
+        ball.collisionDetect()
     }
 
     requestAnimationFrame(loop)
